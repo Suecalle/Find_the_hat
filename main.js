63a13b0b79d4d93c0ecc7fa36d8d97446c141392
Suecalle <hugomenesesl88@gmail.com>
2023-09-06 17:02:09 +0100

    runGame , Ask Question and print methods added

--- main.js
+++ main.js
@@ -10,10 +10,75 @@ class Field {
         this.field = field;
         this.locationY = 0;
         this.locationX = 0;
+        this.field[0][0] = pathCharacter;
+    }
+
+    runGame() {
+        let playing = true;
+        while (playing) {
+            this.print();
+            this.askQuestion();
+            if (this.isInBounds()) {
+                console.log('Out of bounds instructions!');
+                playing = false;
+                break;
+            } else if (this.isHole()) {
+                console.log('Sorry, you fell down a hole!');
+                playing = false;
+                break;
+            } else if (this.isHat()) {
+                console.log('Congrats, you found your hat!');
+                playing = false;
+                break;
+            }
+            this.field[this.locationY][this.locationX] = pathCharacter;
+        }
+    }
+
+    askQuestion() {
+        const answer = prompt('Which way? ').toUpperCase();
+        switch (answer) {
+            case 'S':
+                this.locationY -= 1;
+                break;
+            case 'W':
+                this.locationY += 1;
+                break;
+            case 'A':
+                this.locationX -= 1;
+                break;
+            case 'D':
+                this.locationX += 1;
+                break;
+            default:
+                console.log('Enter W, A, S or D.');
+                this.askQuestion();
+                break;
+        }
+    }
+
+    isInBounds() {
+        return (
+            this.locationY >= 0 &&
+            this.locationX >= 0 &&
+            this.locationY < this.field.length &&
+            this.locationX < this.field[0].length
+        );
+    }
+
+    isHat() {
+        return this.field[this.locationY][thislocationX] === hat;
+    }
+
+    isHole() {
+        return this.field[this.locationY][this.locationX] === hole;
     }
 
     print() {
-        
+        const displayString = this.field.map(row => {
+            return row.join('');
+        }).join('\n');
+        console.log(displayString);
     }
 }
 
@@ -23,7 +88,7 @@ const myField = new Field([
     ['░', '^', '░'],
 ]);
 
-myField.print();
+myField.runGame();
 
 /*let name = prompt('whats your name?');
 console.log(`my name is ${name}`)*/
